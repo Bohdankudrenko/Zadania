43c8b0ff1057667450421905d2d5d7585d60bf9f
Bohdan Kudrenko <bohdan.k20.04@gmail.com>
2025-06-15 22:49:12 +0200

    Commit

--- LAB10/src/Zadanie3/Main.java
+++ LAB10/src/Zadanie3/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Map<String, Double> menu = new LinkedHashMap<>(); // zachowuje kolejność dodawania
+        Map<String, Double> menu = new LinkedHashMap<>();
 
         while (true) {
             System.out.println("\n== MENU ==");
