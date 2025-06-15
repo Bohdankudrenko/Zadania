4e77d88bd14dc4499d3f0773ee969f33c14c0e72
Bohdan Kudrenko <bohdan.k20.04@gmail.com>
2025-06-15 22:34:49 +0200

    Commit

--- LAB7/src/Zadanie3/Main.java
+++ LAB7/src/Zadanie3/Main.java
@@ -5,18 +5,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Reprezentacja wszystkich klas
         Prostokat p1 = new Prostokat(10, 5);
         Trojkat t1 = new Trojkat(6, 4);
         Kwadrat k1 = new Kwadrat(5);
         Okrag o1 = new Okrag(new Punkt(0, 0), 3);
 
-        // Przykładowe funkcje
         p1.skaluj(2);
         System.out.println(t1.opis());
         o1.przesun(3, 4);
 
-        // Tablica figur
         Figura[] tablicaFigur = new Figura[10];
         tablicaFigur[0] = p1;
         tablicaFigur[1] = t1;
@@ -29,7 +26,6 @@ public class Main {
             }
         }
 
-        // Lista IFigury
         List<IFigury> listaFigur = new ArrayList<>();
         listaFigur.add(p1);
         listaFigur.add(k1);
